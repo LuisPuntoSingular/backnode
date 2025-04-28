a7c796f84521d920c25902cd280b7dc28ecd2c02
Luis Enrique <luisenriquep110101@gmail.com>
2025-04-28 15:00:37 -0600

    prueba 3 28/04/2025

--- routes/Employe/employesDocuments.js
+++ routes/Employe/employesDocuments.js
@@ -7,7 +7,7 @@ router.get('/:id', async (req, res) => {
     const { id } = req.params;
     try {
         const result = await pool.query(
-            'SELECT birth_certificate, curp, proof_of_address, ine, rfc FROM employee_documents WHERE id = $1',
+            'SELECT birth_certificate, curp, proof_of_address, ine, rfc, nss FROM employee_documents WHERE id = $1',
             [id]
         );
         if (result.rowCount === 0) {
@@ -20,19 +20,18 @@ router.get('/:id', async (req, res) => {
     }
 });
 
-
 // PUT: Actualizar un documento de empleado por ID
 router.put('/:id', async (req, res) => {
     const { id } = req.params;
-    const { birth_certificate, curp, proof_of_address, ine, rfc } = req.body;
+    const { birth_certificate, curp, proof_of_address, ine, rfc, nss } = req.body;
 
     try {
         const result = await pool.query(
             `UPDATE employee_documents
-             SET birth_certificate = $1, curp = $2, proof_of_address = $3, ine = $4, rfc = $5
-             WHERE id = $6
+             SET birth_certificate = $1, curp = $2, proof_of_address = $3, ine = $4, rfc = $5, nss = $6
+             WHERE id = $7
              RETURNING *`,
-            [birth_certificate, curp, proof_of_address, ine, rfc, id]
+            [birth_certificate, curp, proof_of_address, ine, rfc, nss, id]
         );
 
         if (result.rowCount === 0) {
