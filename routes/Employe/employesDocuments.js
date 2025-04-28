da34ce31a8147873986007d4bdbaf94fdab667d2
Luis Enrique <luisenriquep110101@gmail.com>
2025-04-28 14:39:27 -0600

    prueba 2 28/04/2025

--- routes/Employe/employesDocuments.js
+++ routes/Employe/employesDocuments.js
@@ -2,11 +2,14 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../../db'); // Asegúrate de tener configurada tu conexión a la base de datos
 
-// GET: Obtener todos los documentos de empleados
+// GET: Obtener solo los documentos de un empleado por ID
 router.get('/:id', async (req, res) => {
     const { id } = req.params;
     try {
-        const result = await pool.query('SELECT * FROM employee_documents WHERE id = $1', [id]);
+        const result = await pool.query(
+            'SELECT birth_certificate, curp, proof_of_address, ine, rfc FROM employee_documents WHERE id = $1',
+            [id]
+        );
         if (result.rowCount === 0) {
             return res.status(404).json({ error: 'Documento de empleado no encontrado' });
         }
@@ -16,6 +19,7 @@ router.get('/:id', async (req, res) => {
         res.status(500).json({ error: 'Error al obtener los documentos del empleado' });
     }
 });
+
 
 // PUT: Actualizar un documento de empleado por ID
 router.put('/:id', async (req, res) => {
