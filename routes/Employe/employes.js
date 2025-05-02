ff3f7649d5bbbbafc9be73c7e93e45587291469c
Luis Enrique <luisenriquep110101@gmail.com>
2025-05-02 15:29:24 -0600

    Deploy prueba 1 02/05/2025

--- routes/Employe/employes.js
+++ routes/Employe/employes.js
@@ -14,6 +14,27 @@ router.get("/", async (req, res) => {
   }
 });
 
+
+
+// Obtener un empleado por ID
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const result = await client.query(`
+      SELECT * FROM employees WHERE id = $1
+    `, [id]);
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: "Employee not found" });
+    }
+
+    res.status(200).json(result.rows[0]);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+
 // Crear un nuevo empleado
 router.post("/", async (req, res) => {
   const {
